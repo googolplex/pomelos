7b709006743898c5844cefa62c9212e2ab144f88
Roger Armoa <googolplex@lcompras.biz>
2017-04-21 10:48:50 -0400

    control de stock - inicio abril2017

---
+++ gitfeliz/clonar_pomelosv1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# hecho 04/2017
+# cd  pomelos
+git clone https://github.com/googolplex/pomelos.git
---
+++ gitfeliz/grabar_enviar_limones_v3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# el 2017/04
+cp -r /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/workspace/pomelos  pomelos
+
+cp "/home/xoldfusion/Descargas/openxava-5.6.1_pomelos/workspace/Servers/Tomcat v7.0 Server at localhost-config/server.xml"  /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/
+cp "/home/xoldfusion/Descargas/openxava-5.6.1_pomelos/workspace/Servers/Tomcat v7.0 Server at localhost-config/context.xml" /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/
+cp "/home/xoldfusion/Descargas/openxava-5.6.1_pomelos/workspace/Servers/Tomcat v7.0 Server at localhost-config/web.xml" /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/
+cp /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/server.xml /home/xoldfusion/Descargas/pomelos2017a/tomcatfeliz
+cp /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/context.xml /home/xoldfusion/Descargas/pomelos2017a/tomcatfeliz
+cp /home/xoldfusion/Descargas/openxava-5.6.1_pomelos/tomcat/conf/web.xml /home/xoldfusion/Descargas/pomelos2017a/tomcatfeliz
+cp tomcatfeliz/server.xml  	pomelos/tomcatfeliz/
+cp tomcatfeliz/context.xml  	pomelos/tomcatfeliz/
+cp tomcatfeliz/web.xml  	pomelos/tomcatfeliz/
+cp *.sh pomelos/gitfeliz
+cp  amazon2017llave/*.sh 	pomelos/amazon2017llave/
+rm *.*~
+rm pomelos/*.*~
+cd pomelos
+git add -A
+git commit -a -m "control de stock - inicio abril2017"
+git push -u origin master
